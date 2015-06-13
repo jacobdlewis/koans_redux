de8a057c03a0491c463e5b7b0ef4f991f8af2223
jacobdlewis <jacobdlewis@gmail.com>
2015-06-12 19:22:00 -0500

    triangle part two complete

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if a == b && b == c && a == c
+  if a <= 0 or b <= 0 or c <= 0
+    raise TriangleError, "no sides can be equal or less than zero"
+  elsif a + b <= c || a + c <= b || b + c <= a
+    raise TriangleError, "a + b must be greater than c"
+  elsif a == b && b == c && a == c
     :equilateral
   elsif a == b || b == c || a == c
     :isosceles
